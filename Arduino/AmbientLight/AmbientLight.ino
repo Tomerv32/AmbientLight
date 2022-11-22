dd86d72d2ff1ba6110e66bd3c06ed44f7ee4d27c
Tomerv32 <tomerv32@gmail.com>
2022-11-22 05:58:56 +0200

    Arduino: - baud rate set to 9600 - handle 1 led in a loop - Future: add brightness control

--- Arduino/AmbientLight/AmbientLight.ino
+++ Arduino/AmbientLight/AmbientLight.ino
@@ -2,36 +2,41 @@
 FASTLED_USING_NAMESPACE
 
 #define DATA_PIN    3
-#define LED_TYPE    WS2812
+#define LED_TYPE    WS2811
 #define COLOR_ORDER GRB
-#define NUM_LEDS    46
-#define BRIGHTNESS  96
+#define NUM_LEDS    100
+#define BRIGHTNESS  100
 CRGB leds[NUM_LEDS];
 
-#define BUFFER_SIZE 12
+#define SERIAL_BAUD_RATE 9600
+
+#define VALUES_PER_LED 4
+
+#define BUFFER_SIZE VALUES_PER_LED
 byte buff[BUFFER_SIZE];
 
+
 void setup() {
-  Serial.begin(250000);
+  Serial.begin(SERIAL_BAUD_RATE);
 
   FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(BRIGHTNESS);
   // FastLED.setMaxRefreshRate(0, false);  // turn OFF the refresh rate constraint
-
 }
 
-
 void loop() {
+  // int brightness = analogRead(pot_pin);
+  // map potentiometer value to 0-255
+  // FastLED.setBrightness(brightness);
+
   if (Serial.available())
   {
     Serial.readBytes(buff, BUFFER_SIZE);
-    for (int i=0; i<BUFFER_SIZE; i=i+4)
-    {
-      leds[int(buff[i])].r = int(buff[i+1]);
-      leds[int(buff[i])].g = int(buff[i+2]);
-      leds[int(buff[i])].b = int(buff[i+3]);
-    }
+
+    leds[int(buff[0])].r = int(buff[1]);
+    leds[int(buff[0])].g = int(buff[2]);
+    leds[int(buff[0])].b = int(buff[3]);
+
     FastLED.show();
-    // delay(50);
   }
 }
